516d3b1553626fb084e00caeafc812772cd92396
Pratik Thakkar <thakkarpratik277@gmail.com>
2022-05-01 22:39:38 +0530

    Updated wrapper script as per challenge requirement. options without dashes (-/--) supported.

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -6,9 +6,9 @@ function usage
         cat << EOT
 Usage:
 
--p, --produce                            Build the documentation for website
--s, --serve                              Run the built-in dev-server that lets you preview your documentation
--h, --help                               Help about supported command
+-p, produce                            Build the documentation for website
+-s, serve                              Run the built-in dev-server that lets you preview your documentation
+-h, help                               Help about supported command
 
 EOT
 }
@@ -16,44 +16,40 @@ EOT
 
 # Parse Command Line Arguments
 
-OPTIONS=`getopt -o psh --long produce,serve,help -- "$@"`
 if [ $? -ne 0 ]; then
   usage
   exit 1
 fi
 
-eval set -- "$OPTIONS"
-while true; do
-  case "$1" in
-    -p|--produce)
-        param="produce";
-        shift;;
-    -s|--serve)
-        param="serve";
-        shift;;
-    --)
-        shift; break;;
-    -h|--help)
-        usage;
-        exit 0;;
-    *)
-      echo "Unexpected argument: $1"
-      usage
-      exit 1;;
-  esac
-done
+while test $# -gt 0; do
+        param="$1"
+        
+        if test "${1::1}" = "-"; then
+                if test ${#1} -gt 2 -a "${1::2}" = "--" ; then
+                        param="${1:2}"
+                else
+                        param="${1:1}"
+                fi
+        else
+                param="$1"
+        fi
+        shift
 
-case "$param" in
-  serve)
-    #echo "In Serve"
-    mkdocs serve --livereload -a "0.0.0.0:8000"
-    exit 0;;
-  produce)
-    #echo "In produce"
-    mkdocs build -c -d /docs/output
-    exit 0;;
-  *)
-    echo "Unexpected command: $param"
-    usage
-    exit 1;;
-esac
+        case "$param" in
+        serve|s)
+            tar -xzf /docs/src/mkdocs.tar.gz -C /docs/src/
+            mkdocs serve --livereload -a "0.0.0.0:8000"
+            exit 0;;
+        produce|p)
+            mkdocs build -c -d /docs/src/site
+            cd /docs/src; tar -czf /docs/output/mkdocs.tar.gz /docs/src/site/index.html *
+            exit 0;;
+        help|h)
+            usage
+            exit 0;;
+        *)
+            echo "Unexpected command: $OPT"
+            usage
+            exit 1;;
+        esac
+done
